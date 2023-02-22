af122389d7ae65cf9fdf04aed9cd9a38511f8e14
Caiden Palmer (THS Student) <17palmer.c@turinghouseschool.org.uk>
2023-02-22 15:49:23 +0000

    hi

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Console
+{
+    class Pets
+    {
+        private bool hair { get;}
+        private String hairCol{ get;}
+        private double size{ get; set;}
+        private String sound{ get;}
+        private String food{ get;}
+        private String nationality{ get;}
+        private String breed{ get;}
+        public Pets(bool hair, String hairCol, double size, string sound, string food, string nationality, string breed)
+        {
+            this.hair = hair;
+            this.hairCol = hairCol;
+            this.size = size;
+            this.sound = sound;
+            this.food = food;
+            this.nationality = nationality;
+            this.breed = breed;
+        }
+        public 
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
