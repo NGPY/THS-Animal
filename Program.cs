9b97eca2aed0f754d944f0e8ca0eadeb71443bf3
CDog23 <nzcjpalmer3@gmail.com>
2023-02-23 11:30:36 +0000

    My neck my back

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 {
     class Pets
     {
+        private bool sleepy { get;}
         private bool hair { get;}
         private string hairCol{ get;}
         private double size;
@@ -9,6 +10,7 @@
         private string food{ get;}
         private string nationality{ get;}
         private string breed{ get;}
+        
         public Pets(bool hair, string hairCol, double size, string sound, string food, string nationality, string breed)
         {
             this.hair = hair;
@@ -19,11 +21,20 @@
             this.nationality = nationality;
             this.breed = breed;
         }
+        public void Status()
+        {
+            System.Console.WriteLine($"Size: {size}, Sleepy? {sleepy == 1?"Yes":"No"}");
+        }
+        public int Sleepy()
+        {
+            Random rng = new Random();
+            sleepy = rng.Next(0,1);
+        }
 
         public void Feed(string food)
         {
             System.Console.WriteLine("You have fed your pet");
-            System.Console.WriteLine($"Your pet has grown {this.size * 1.03 - this.size} cm!");
+            System.Console.WriteLine($"Your pet has grown {this.size * 1.03 - this.size} cm cubed!");
             this.size = this.size * 1.03;
         }
         public double GetSize()
@@ -50,16 +61,27 @@
         {
             cheekiness = cheek;
         }
+        public void Cheeky(int cheeky)
+        {
+            if (cheeky != cheekiness)
+            {
+                cheekiness = cheeky;
+            }
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Kangaroo dog = new Kangaroo(false, null, 242534534, "woof", "children", "Australia", "Australian Kangaroo", 1, 0.02);
+            String[] colours = new string[] {"brown", "black", "white", "albino", "pink", "purple", "orange", "red", "green", "yellow"}
+            string[] food = new string[] {""}
+            Kangaroo dog = new Kangaroo(true, "brown", 100, "aooga", "children", "Australia", "Australian Kangaroo", 1, 0.02);
             System.Console.WriteLine($"Size: {dog.GetSize()}");
             dog.Feed("");
             System.Console.WriteLine($"Size: {dog.GetSize().ToString("#.##")}");
+            dog.Sleepy();
+            System.Console.WriteLine(dog.Status());
             System.Console.ReadLine();
         }
     }
